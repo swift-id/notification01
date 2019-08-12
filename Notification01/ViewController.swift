211e5486302146e368eee8bba18555381a9c6b25
R. Kukuh <rkukuh@gmail.com>
2019-08-13 06:50:30 +0700

    Initial Commit

---
+++ Notification01/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Notification01
+//
+//  Created by R. Kukuh on 13/08/19.
+//  Copyright © 2019 R. Kukuh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
